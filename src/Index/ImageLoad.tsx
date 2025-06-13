44a75224c9fd66ae6d379eb00b14aad40674e988
amo-yotakenoko <amo-yotakenoko@gmail.com>
2025-06-14 00:41:26 +0900

    メニュー無効化

--- src/Index/ImageLoad.tsx
+++ src/Index/ImageLoad.tsx
@@ -1,175 +1,171 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 const ImageLoad: React.FC = () => {
-    const navigate = useNavigate();
-    useEffect(() => {
-        const selFile = document.getElementById('inputFile') as HTMLInputElement;
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    const selFile = document.getElementById("inputFile") as HTMLInputElement;
 
+    if (selFile) {
+      selFile.addEventListener("change", function (evt) {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-        if (selFile) {
-            selFile.addEventListener("change", function (evt) {
-                const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
 
-                const ctx = canvas.getContext('2d');
-                if (!ctx) return;
+        const target = evt.target as HTMLInputElement;
+        if (!target.files) return;
 
-                const target = evt.target as HTMLInputElement;
-                if (!target.files) return;
-
-                const file = target.files[0];
-                if (!file) {
-                    console.error("ファイルがない", file);
-                    return;
-                }
-
-                console.log(file);
-
-                const reader = new FileReader();
-                reader.readAsDataURL(file);
-                reader.onload = function (e) {
-                    const dataUrl = e.target?.result as string;
-                    if (typeof dataUrl === 'string') {
-                        const img = new Image();
-                        img.src = dataUrl;
-                        img.onload = function () {
-                            canvas.width = img.width;
-                            canvas.height = img.height;
-                            canvas.style.display = 'block';
-                            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-                            console.log("aaaa")
-                            try {
-                                let restoredData = binLoad();
-                                restoredData.saveKeyName = `image-${new Date().toLocaleTimeString()}`;
-                                navigate('/edit', { state: restoredData });
-                            } catch (error) {
-                                console.log("新しい形式だめ", error);
-                                try {
-                                    let restoredData = binLoad_old();
-                                    restoredData.saveKeyName = `image-${new Date().toLocaleTimeString()}`;
-                                    navigate('/edit', { state: restoredData });
-                                } catch (error) {
-                                    console.log("古い形式だめ", error);
-                                    alert(`読み取れませんでした: ${error}`);
-                                    // const buttonElement = document.getElementById('imageToLoadHelpBUtton') as HTMLElement;
-                                    // console.log(buttonElement);
-                                    //   buttonElement?.click();
-
-// // 要素が存在するかを確認してからクリック
-// if (buttonElement) {
-//     buttonElement.click(); // 要素が存在する場合にクリック
-// } else {
-//     console.error('要素が見つかりませんでした。IDが正しいか確認してください。');
-// }
-                                }
-                            }
-                            // binLoad();
-                        };
-                    }
-                };
-
-                function binLoad() {
-                    if (!ctx) return;
-                    var ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                    console.log({ ImageData })
-                    let image_i = 0
-                    let bits: any = [];
-                    for (let i = 0; i < canvas.width * canvas.height; i += 1) {
-                        // if (ImageData.data[image_i + 1] != 0) break;
-                        // bin.push(ImageData.data[image_i])
-                        bits.push(ImageData.data[image_i] > (255 / 2) ? 1 : 0)
-                        image_i += 4
-                        const x = (image_i / 4) % 900;
-                        if (x >= 32) {
-                            image_i += (900 - x) * 4;
-                        }
-
-                    }
-                    console.log("bits", bits)
-                    let bin: any = []
-                    for (let i = 0; i < bits.length; i += 8) {
-                        // 8ビットを1バイトに変換
-                        const byte = parseInt(bits.slice(i, i + 8).join(''), 2);
-                        bin.push(byte);
-                    }
-                    console.log("bin", bin)
-
-
-
-                    ctx.putImageData(ImageData, 0, 0);
-                    let text = new TextDecoder().decode(new Uint8Array(bin));
-                    console.log("text", text)
-                    let textEnd = 0;
-                    let bracketCount = 0;
-                    for (const c of text) {
-                        console.log(c);
-                        if (c == "{") bracketCount += 1;
-                        if (c == "[") bracketCount += 1;
-                        if (c == "(") bracketCount += 1;
-                        if (c == "}") bracketCount -= 1;
-                        if (c == "]") bracketCount -= 1;
-                        if (c == ")") bracketCount -= 1;
-                        textEnd += 1;
-                        if (bracketCount == 0) {
-                            console.log("おわり", textEnd)
-                            break;
-                        }
-
-                    }
-                    text = text.substring(0, textEnd);
-                    console.log(text)
-
-                    return JSON.parse(text);
-                }
-
-
-                function binLoad_old() {
-                    let bin = []
-                    if (!ctx) return;
-                    var ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                    console.log({ ImageData })
-                    let image_i = 0
-                    for (let i = 0; i < canvas.width * canvas.height * 4; i += 1) {
-                        if (ImageData.data[image_i + 1] != 0) break;
-                        bin.push(ImageData.data[image_i])
-                        image_i += 4
-
-                    }
-                    console.log("bin", bin)
-                    ctx.putImageData(ImageData, 0, 0);
-                    let text = new TextDecoder().decode(new Uint8Array(bin));
-                    console.log("text", text)
-
-                    return JSON.parse(text);
-                }
-
-
-            });
+        const file = target.files[0];
+        if (!file) {
+          console.error("ファイルがない", file);
+          return;
         }
-    }, []);
 
+        console.log(file);
 
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = function (e) {
+          const dataUrl = e.target?.result as string;
+          if (typeof dataUrl === "string") {
+            const img = new Image();
+            img.src = dataUrl;
+            img.onload = function () {
+              canvas.width = img.width;
+              canvas.height = img.height;
+              canvas.style.display = "block";
+              ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+              console.log("aaaa");
+              try {
+                let restoredData = binLoad();
+                restoredData.saveKeyName = `image-${new Date().toLocaleTimeString()}`;
+                navigate(`/edit${location.search}`, {
+                  state: restoredData,
+                });
+              } catch (error) {
+                console.log("新しい形式だめ", error);
+                try {
+                  let restoredData = binLoad_old();
+                  restoredData.saveKeyName = `image-${new Date().toLocaleTimeString()}`;
+                  navigate(`/edit${location.search}`, { state: restoredData });
+                } catch (error) {
+                  console.log("古い形式だめ", error);
+                  alert(`読み取れませんでした: ${error}`);
+                  // const buttonElement = document.getElementById('imageToLoadHelpBUtton') as HTMLElement;
+                  // console.log(buttonElement);
+                  //   buttonElement?.click();
 
+                  // // 要素が存在するかを確認してからクリック
+                  // if (buttonElement) {
+                  //     buttonElement.click(); // 要素が存在する場合にクリック
+                  // } else {
+                  //     console.error('要素が見つかりませんでした。IDが正しいか確認してください。');
+                  // }
+                }
+              }
+              // binLoad();
+            };
+          }
+        };
 
+        function binLoad() {
+          if (!ctx) return;
+          var ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+          console.log({ ImageData });
+          let image_i = 0;
+          let bits: any = [];
+          for (let i = 0; i < canvas.width * canvas.height; i += 1) {
+            // if (ImageData.data[image_i + 1] != 0) break;
+            // bin.push(ImageData.data[image_i])
+            bits.push(ImageData.data[image_i] > 255 / 2 ? 1 : 0);
+            image_i += 4;
+            const x = (image_i / 4) % 900;
+            if (x >= 32) {
+              image_i += (900 - x) * 4;
+            }
+          }
+          console.log("bits", bits);
+          let bin: any = [];
+          for (let i = 0; i < bits.length; i += 8) {
+            // 8ビットを1バイトに変換
+            const byte = parseInt(bits.slice(i, i + 8).join(""), 2);
+            bin.push(byte);
+          }
+          console.log("bin", bin);
 
-    return (
-        <div>
-            <input type="file" id="inputFile" className="form-control" accept="image/*" />
-            <canvas id="canvas" style={{
+          ctx.putImageData(ImageData, 0, 0);
+          let text = new TextDecoder().decode(new Uint8Array(bin));
+          console.log("text", text);
+          let textEnd = 0;
+          let bracketCount = 0;
+          for (const c of text) {
+            console.log(c);
+            if (c == "{") bracketCount += 1;
+            if (c == "[") bracketCount += 1;
+            if (c == "(") bracketCount += 1;
+            if (c == "}") bracketCount -= 1;
+            if (c == "]") bracketCount -= 1;
+            if (c == ")") bracketCount -= 1;
+            textEnd += 1;
+            if (bracketCount == 0) {
+              console.log("おわり", textEnd);
+              break;
+            }
+          }
+          text = text.substring(0, textEnd);
+          console.log(text);
 
-                border: "2px solid black",
-                width: "100%",
-                display: 'none'
-                // // display: "block",
-                // margin: "auto"
-            }} ></canvas>
-        </div>
-    );
+          return JSON.parse(text);
+        }
+
+        function binLoad_old() {
+          let bin = [];
+          if (!ctx) return;
+          var ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+          console.log({ ImageData });
+          let image_i = 0;
+          for (let i = 0; i < canvas.width * canvas.height * 4; i += 1) {
+            if (ImageData.data[image_i + 1] != 0) break;
+            bin.push(ImageData.data[image_i]);
+            image_i += 4;
+          }
+          console.log("bin", bin);
+          ctx.putImageData(ImageData, 0, 0);
+          let text = new TextDecoder().decode(new Uint8Array(bin));
+          console.log("text", text);
+
+          return JSON.parse(text);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="file"
+        id="inputFile"
+        className="form-control"
+        accept="image/*"
+      />
+      <canvas
+        id="canvas"
+        style={{
+          border: "2px solid black",
+          width: "100%",
+          display: "none",
+          // // display: "block",
+          // margin: "auto"
+        }}
+      ></canvas>
+    </div>
+  );
 };
 
 export default ImageLoad;
 
-
 // [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,
 // [1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0,
 // 元
-// [123, 34, 112, 97, 116, 116, 101, 114, 110, 34, 58, 91, 91, 49, 44, 49, 44, 49, 44, 49, 44, 50, 44, 50, 44, 50, 44, 49, 93, 44, 91, 50, 44, 49, 44, 49, 44, 50, 44, 49, 44, 49, 44, 50, 44, 49, 93, 44, 91, 49, 44, 49, 44, 50, 44, 50, 44, 49, 44, 48, 44, 49, 44, 49, 93, 44, 91, 49, 44, 50, 44, 50, 44, 50, 44, 49, 44, 48, 44, 50, 44, 49, 93, 44, 91, 49, 44, 49, 44, 50, 44, 50, 44, 50, 44, 49, 44, 50, 44, 49, 
+// [123, 34, 112, 97, 116, 116, 101, 114, 110, 34, 58, 91, 91, 49, 44, 49, 44, 49, 44, 49, 44, 50, 44, 50, 44, 50, 44, 49, 93, 44, 91, 50, 44, 49, 44, 49, 44, 50, 44, 49, 44, 49, 44, 50, 44, 49, 93, 44, 91, 49, 44, 49, 44, 50, 44, 50, 44, 49, 44, 48, 44, 49, 44, 49, 93, 44, 91, 49, 44, 50, 44, 50, 44, 50, 44, 49, 44, 48, 44, 50, 44, 49, 93, 44, 91, 49, 44, 49, 44, 50, 44, 50, 44, 50, 44, 49, 44, 50, 44, 49,
--- src/Index/LocalStrageLoad.tsx
+++ src/Index/LocalStrageLoad.tsx
@@ -1,156 +1,175 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, ProgressBar } from 'react-bootstrap';
-import addIcon from '../img/designsystem-assets/icon/png/add_fill24.png';
-import Modal from 'react-bootstrap/Modal';
-import * as Icon from 'react-bootstrap-icons';
-interface LocalStrageLoadProps { }
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, ProgressBar } from "react-bootstrap";
+import addIcon from "../img/designsystem-assets/icon/png/add_fill24.png";
+import Modal from "react-bootstrap/Modal";
+import * as Icon from "react-bootstrap-icons";
+interface LocalStrageLoadProps {}
 
-const LocalStrageLoad: React.FC<LocalStrageLoadProps> = ({ }) => {
-    const [data, setData] = useState<any>({});
-    const [strageUsed, setStrageUsed] = useState(0);
-    const [items, setItems] = useState<any[]>([]);
-    const navigate = useNavigate();
-    const [deleteConfirmation, setDeleteConfirmation] = useState<any>(undefined);
-    useEffect(() => {
-        // const loadData = () => {
-        let value = localStorage.getItem('pompoms');
-        if (value !== null) {
-            let datas = JSON.parse(value);
-            console.log({ datas });
-            const newItems = [];
-            // for (const key in datas) {
-            //     const value = datas[key];
-            //     newItems.push(
-            //         <div key={key} className="col-6 col-md-3 mb-3 d-flex">
-            //             <div className="card flex-fill">
-            //                 <div className="card-body" style={{ padding: "10px" }}>
-            //                     <div style={{ position: 'relative', width: '100%' }}>
-            //                         <img
-            //                             src={addIcon}
-            //                             style={{
-            //                                 right: "0px",
-            //                                 top: "0px",
-            //                                 transform: 'rotate(45deg)',
-            //                                 position: 'absolute',
-            //                             }}
-            //                             onClick={() => setDeleteConfirmation(key)}
-            //                             alt="Delete Icon"
-            //                         />
-            //                         <img src={value.img} style={{ width: '100%' }} alt="Sample" />
-            //                         <Button
-            //                             style={{
-            //                                 position: 'absolute',
-            //                                 bottom: '0px',
-            //                                 right: '0px',
-            //                                 zIndex: 1
-            //                             }}
-            //                             onClick={() => navigeteEdit(key, value.data)}
-            //                         >
-            //                             編集
-            //                         </Button>
-            //                     </div>
-            //                 </div>
-            //             </div>
-            //         </div>
-            //     );
-            // }
-            // setItems(newItems.reverse());
-            setData(datas);
-
-        }
-        // };
-        // loadData();
-    }, []);
-
-
-    useEffect(() => {
-        const newItems = [];
-        for (const key in data) {
-            const value = data[key];
-            newItems.push(
-                <div key={key} className="col-6 col-md-3 mb-3 d-flex">
-                    <div className="card flex-fill">
-                        <div className="card-body" style={{ padding: "10px" }}>
-                            <div style={{ position: 'relative', width: '100%' }}>
-                                <img src={value.img} style={{ width: '100%' , mixBlendMode: 'multiply'}} />
-                                <img
-                                    src={addIcon}
-                                    style={{
-                                        right: "0px",
-                                        top: "0px",
-                                        transform: 'rotate(45deg)',
-                                        position: 'absolute',
-                                    }}
-                                    onClick={() => setDeleteConfirmation(key)}
-                                    alt="Delete Icon"
-                                />
-                                <Icon.Pencil
-                                    style={{
-                                        position: 'absolute',
-                                        bottom: '0px',
-                                        right: '0px',
-                                        zIndex: 1,
-                                        fontSize: '2em'
-                                    }}
-                                    onClick={() => navigeteEdit(key, value.data)}
-                                >
-
-                                </Icon.Pencil>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-        setItems(newItems.reverse());
-        setStrageUsed(new TextEncoder().encode(JSON.stringify(data)).length / (1024 * 1024))
-    }, [data]);
-
-    function navigeteEdit(key: any, data: any) {
-        console.log(data);
-        data.saveKeyName = key;
-        navigate('/edit', { state: data });
+const LocalStrageLoad: React.FC<LocalStrageLoadProps> = ({}) => {
+  const [data, setData] = useState<any>({});
+  const [strageUsed, setStrageUsed] = useState(0);
+  const [items, setItems] = useState<any[]>([]);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [deleteConfirmation, setDeleteConfirmation] = useState<any>(undefined);
+  useEffect(() => {
+    // const loadData = () => {
+    let value = localStorage.getItem("pompoms");
+    if (value !== null) {
+      let datas = JSON.parse(value);
+      console.log({ datas });
+      const newItems = [];
+      // for (const key in datas) {
+      //     const value = datas[key];
+      //     newItems.push(
+      //         <div key={key} className="col-6 col-md-3 mb-3 d-flex">
+      //             <div className="card flex-fill">
+      //                 <div className="card-body" style={{ padding: "10px" }}>
+      //                     <div style={{ position: 'relative', width: '100%' }}>
+      //                         <img
+      //                             src={addIcon}
+      //                             style={{
+      //                                 right: "0px",
+      //                                 top: "0px",
+      //                                 transform: 'rotate(45deg)',
+      //                                 position: 'absolute',
+      //                             }}
+      //                             onClick={() => setDeleteConfirmation(key)}
+      //                             alt="Delete Icon"
+      //                         />
+      //                         <img src={value.img} style={{ width: '100%' }} alt="Sample" />
+      //                         <Button
+      //                             style={{
+      //                                 position: 'absolute',
+      //                                 bottom: '0px',
+      //                                 right: '0px',
+      //                                 zIndex: 1
+      //                             }}
+      //                             onClick={() => navigeteEdit(key, value.data)}
+      //                         >
+      //                             編集
+      //                         </Button>
+      //                     </div>
+      //                 </div>
+      //             </div>
+      //         </div>
+      //     );
+      // }
+      // setItems(newItems.reverse());
+      setData(datas);
     }
+    // };
+    // loadData();
+  }, []);
 
-    function deleteItem(key: any) {
-        console.log(key, "を削除");
-        let value = localStorage.getItem('pompoms');
-        if (value == null) return;
-        let datas = JSON.parse(value);
-        delete datas[key];
-        localStorage.setItem('pompoms', JSON.stringify(datas));
-        setData(datas);
-    }
-console.log(`${strageUsed.toFixed(2)}MB/5MB`)
-    return (
-        <>
-            {/* <div className="row"> */}
-            {strageUsed / 5 * 100 > 35 && <div className="col-12 my-3">
-                <ProgressBar now={strageUsed / 5 * 100} label={`${strageUsed.toFixed(2)}MB/5MB`}
-                    variant={
-                        strageUsed / 5 > 0.75 ? 'danger' :
-                            strageUsed / 5 > 0.5 ? 'warning' :
-                                'primary'}
+  useEffect(() => {
+    const newItems = [];
+    for (const key in data) {
+      const value = data[key];
+      newItems.push(
+        <div key={key} className="col-6 col-md-3 mb-3 d-flex">
+          <div className="card flex-fill">
+            <div className="card-body" style={{ padding: "10px" }}>
+              <div style={{ position: "relative", width: "100%" }}>
+                <img
+                  src={value.img}
+                  style={{ width: "100%", mixBlendMode: "multiply" }}
                 />
-            </div>}
-            {items}
-            {/* </div> */}
-            <Modal show={deleteConfirmation != undefined} onHide={() => { setDeleteConfirmation(undefined) }}>
-                <Modal.Header closeButton>
-                    <Modal.Title>削除しますか?</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{deleteConfirmation}</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => { deleteItem(deleteConfirmation); setDeleteConfirmation(undefined) }}>
-                        OK
-                    </Button>
-
-                </Modal.Footer>
-            </Modal>
-        </>
+                <img
+                  src={addIcon}
+                  style={{
+                    right: "0px",
+                    top: "0px",
+                    transform: "rotate(45deg)",
+                    position: "absolute",
+                  }}
+                  onClick={() => setDeleteConfirmation(key)}
+                  alt="Delete Icon"
+                />
+                <Icon.Pencil
+                  style={{
+                    position: "absolute",
+                    bottom: "0px",
+                    right: "0px",
+                    zIndex: 1,
+                    fontSize: "2em",
+                  }}
+                  onClick={() => navigeteEdit(key, value.data)}
+                ></Icon.Pencil>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    setItems(newItems.reverse());
+    setStrageUsed(
+      new TextEncoder().encode(JSON.stringify(data)).length / (1024 * 1024)
     );
+  }, [data]);
+
+  function navigeteEdit(key: any, data: any) {
+    console.log(data);
+    data.saveKeyName = key;
+    navigate(`/edit${location.search}`, { state: data });
+  }
+
+  function deleteItem(key: any) {
+    console.log(key, "を削除");
+    let value = localStorage.getItem("pompoms");
+    if (value == null) return;
+    let datas = JSON.parse(value);
+    delete datas[key];
+    localStorage.setItem("pompoms", JSON.stringify(datas));
+    setData(datas);
+  }
+  console.log(`${strageUsed.toFixed(2)}MB/5MB`);
+  return (
+    <>
+      {/* <div className="row"> */}
+      {(strageUsed / 5) * 100 > 35 && (
+        <div className="col-12 my-3">
+          <ProgressBar
+            now={(strageUsed / 5) * 100}
+            label={`${strageUsed.toFixed(2)}MB/5MB`}
+            variant={
+              strageUsed / 5 > 0.75
+                ? "danger"
+                : strageUsed / 5 > 0.5
+                ? "warning"
+                : "primary"
+            }
+          />
+        </div>
+      )}
+      {items}
+      {/* </div> */}
+      <Modal
+        show={deleteConfirmation != undefined}
+        onHide={() => {
+          setDeleteConfirmation(undefined);
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>削除しますか?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{deleteConfirmation}</Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              deleteItem(deleteConfirmation);
+              setDeleteConfirmation(undefined);
+            }}
+          >
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
 
 export default LocalStrageLoad;
